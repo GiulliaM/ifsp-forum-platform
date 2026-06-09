dacce115343244dec526a92df9b1d74fb344a54f
GiulliaM <giulliameneses@gmail.com>
2026-06-09 17:51:56 -0300

    feat: estrutura inicial dos microsserviços

---
+++ algorithm-service/src/main/java/br/edu/ifsp/guarulhos/algorithm_service/AlgorithmServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.guarulhos.algorithm_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmServiceApplication.class, args);
+	}
+
+}
---
+++ forum-service/src/test/java/br/edu/ifsp/guarulhos/forum_service/ForumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.guarulhos.forum_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
