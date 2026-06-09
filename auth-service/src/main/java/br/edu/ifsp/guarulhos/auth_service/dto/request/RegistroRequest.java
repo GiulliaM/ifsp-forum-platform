b17355c537030224d1eb00a20760579bbf7db587
GiulliaM <giulliameneses@gmail.com>
2026-06-09 18:18:29 -0300

    feat: novas classes dtos

---
+++ auth-service/src/main/java/br/edu/ifsp/guarulhos/auth_service/dto/request/RegistroRequest.java
@@ -0,0 +1,25 @@
+package br.edu.ifsp.guarulhos.auth_service.dto.request;
+
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegistroRequest {
+
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
+    private String senha;
+
+    @AssertTrue(message = "Você deve aceitar os termos de uso")
+    private boolean termosAceitos;
+}
---
+++ auth-service/src/main/java/br/edu/ifsp/guarulhos/auth_service/dto/response/AuthResponse.java
@@ -0,0 +1,15 @@
+package br.edu.ifsp.guarulhos.auth_service.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class AuthResponse {
+
+    private String token;
+    private String nome;
+    private String email;
+    private String role;
+}
