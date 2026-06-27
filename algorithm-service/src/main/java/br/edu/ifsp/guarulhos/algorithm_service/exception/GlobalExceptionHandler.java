b46dd22a8b51428501a79af14856366763e9838f
GiulliaM <giulliameneses@gmail.com>
2026-06-27 00:10:24 -0300

    feat: handler trata erros comuns do spring e erro inesperado no algorithm_service

--- algorithm-service/src/main/java/br/edu/ifsp/guarulhos/algorithm_service/exception/GlobalExceptionHandler.java
+++ algorithm-service/src/main/java/br/edu/ifsp/guarulhos/algorithm_service/exception/GlobalExceptionHandler.java
@@ -1,21 +1,29 @@
 package br.edu.ifsp.guarulhos.algorithm_service.exception;
 
 import br.edu.ifsp.guarulhos.algorithm_service.dto.response.ErroResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 import java.util.stream.Collectors;
 
 /**
- * Captura as exceções lançadas pelas services e pela validação dos DTOs, devolvendo uma
- * resposta JSON padronizada com o status HTTP correto.
+ * Captura as exceções lançadas pelas services, pela validação dos DTOs e pelos erros
+ * comuns do Spring, devolvendo sempre um JSON padronizado com o status HTTP correto,
+ * em vez da página de erro padrão do framework.
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(RecursoNaoEncontradoException.class)
     public ResponseEntity<ErroResponse> tratarNaoEncontrado(RecursoNaoEncontradoException ex){
@@ -38,6 +46,27 @@ public class GlobalExceptionHandler {
                 .map(erro -> erro.getDefaultMessage())
                 .collect(Collectors.joining("; "));
         return montar(HttpStatus.BAD_REQUEST, mensagem);
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ErroResponse> tratarHeaderAusente(MissingRequestHeaderException ex){
+        return montar(HttpStatus.BAD_REQUEST, "Cabeçalho obrigatório ausente: " + ex.getHeaderName());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErroResponse> tratarCorpoInvalido(HttpMessageNotReadableException ex){
+        return montar(HttpStatus.BAD_REQUEST, "Corpo da requisição inválido ou mal formatado");
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErroResponse> tratarParametroInvalido(MethodArgumentTypeMismatchException ex){
+        return montar(HttpStatus.BAD_REQUEST, "Valor inválido para o parâmetro '" + ex.getName() + "'");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErroResponse> tratarErroInesperado(Exception ex){
+        log.error("Erro inesperado", ex);
+        return montar(HttpStatus.INTERNAL_SERVER_ERROR, "Ocorreu um erro inesperado. Tente novamente mais tarde.");
     }
 
     private ResponseEntity<ErroResponse> montar(HttpStatus status, String mensagem){
